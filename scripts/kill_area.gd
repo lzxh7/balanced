22ae7faefe18fb324b8f300d5f4a8854517ab555
lzxh7 <152106812+lzxh7@users.noreply.github.com>
2023-12-18 20:53:02 -0800

    web export

--- scripts/kill_area.gd
+++
@@ -1,8 +0,0 @@
-class_name KillArea
-extends Area2D
-
-func _ready() -> void:
-	body_entered.connect(_on_body_entered)
-
-func _on_body_entered(body: Node2D) -> void:
-	body.queue_free()
--- scripts/pivot.gd
+++
@@ -1,7 +0,0 @@
-@tool
-extends Node2D
-
-@export var reset_pan_rotations: bool:
-	set(_value):
-		$Pan1.rotation = -rotation
-		$Pan2.rotation = -rotation
